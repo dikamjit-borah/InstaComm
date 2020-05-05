16b98f5bbfd6cda9995f4e4c805192a972bc9501
Dikamjit Borah <36004021+hunkyxstudman@users.noreply.github.com>
2020-05-05 11:04:24 +0530

    Update main.py
    Rebuilt the entire codebase to enhance developer readability and user experience

--- main.py
+++ main.py
@@ -5,150 +5,140 @@ from selenium.webdriver.common.by import By
 import time
 import csv
 
-
-
-'''
-#search on google, and open instagram
-open_Browser.get('https://www.google.com')
-
-search_box = open_Browser.find_element_by_name('q')
-
-search_box.send_keys("instagram")
-time.sleep(2)
-#submit_btn = open_Browser.find_element_by_name('btnK')
-submit_button = open_Browser.find_element_by_css_selector("input[type='submit']")
-#print(submit_btn.get_attribute("name"))
-time.sleep(2)
-submit_button.click()
-insta = open_Browser.find_element_by_xpath("/html/body/div[6]/div[2]/div[9]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/a/h3")
-time.sleep(2)
-insta.click()'''
-
-
-user_name = 'travislay0102'
+user_name = ''
 password = ''
+profile = ''
 
-
-open_Browser = webdriver.Chrome()
-
-open_Browser.get('https://www.instagram.com/')
-time.sleep(3)
-insta_username = open_Browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input")
-insta_username.send_keys(user_name)
-insta_password = open_Browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input")
-insta_password.send_keys(password)
-time.sleep(3)
-insta_submit = open_Browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button")
-insta_submit.click()    
-time.sleep(2)
-
-'''
-#not_now = open_Browser.find_element_by_xpath("//button[@class='aOOlW   HoLwm ']")
-WebDriverWait(open_Browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@class='aOOlW   HoLwm ']"))).click()
-# WebDriverWait(open_Browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.aOOlW.HoLwm"))).click()
-
-time.sleep(3)
-#not_now.click()
-
-'''
-
-ui.WebDriverWait(open_Browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".aOOlW.HoLwm"))).click()
-
-search_for = open_Browser.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input")
-search_for.send_keys("antripa003")
-time.sleep(3)
-ant = open_Browser.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a")
-time.sleep(3)
-ant.click()
-
-time.sleep(3)
-'''
-followers = open_Browser.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/ul/li[2]/a")
-following = open_Browser.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/ul/li[3]/a")
-time.sleep(2)
-
-following.click()
-time.sleep(3)
-
-
-#scroll_box = ui.WebDriverWait(open_Browser, 5).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div/div[2]")))
-
-time.sleep(2)
-scroll_box = open_Browser.find_element_by_xpath("/html/body/div[4]/div/div[2]")
-last_ht, ht = 0, 1
-while last_ht != ht:
-    last_ht = ht
-    time.sleep(1)
-    ht = open_Browser.execute_script("""
-                arguments[0].scrollTo(0, arguments[0].scrollHeight); 
-                return arguments[0].scrollHeight;
-                """, scroll_box)
-#scroll_box = open_Browser.find_element_by_xpath("/html/body/div[3]/div/div[2]")
-'''
-
-recent_post = open_Browser.find_element_by_xpath("/html/body/div[1]/section/main/div/div[3]/article/div[1]/div/div[1]/div[1]/a/div/div[2]")
-time.sleep(2)
-recent_post.click()
-time.sleep(2)
-likes = open_Browser.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[2]/section[2]/div/div/button")
-time.sleep(2)
-likes.click()
-time.sleep(3)
-scroll_box = open_Browser.find_element_by_xpath("/html/body/div[5]/div/div[2]/div")
-last_ht, ht = 0, 1
-
-
+followers_list = []
+following_list = []
 likers = []
-while last_ht != ht:
-    last_ht = ht
-    time.sleep(1)
-    likers_links = scroll_box.find_elements_by_tag_name('a')
-    for link in likers_links:
-        likers_name = link.text
-        if(likers_name!=''):
-            print(likers_name)
-            likers.append(likers_name)
-    ht = open_Browser.execute_script("""
+
+def sleep(t):
+    time.sleep(t)
+
+def login(u_name, p_word):
+    insta_username = open_Browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input")
+    insta_username.send_keys(u_name)
+    insta_password = open_Browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input")
+    insta_password.send_keys(p_word)
+    time.sleep(2)   
+    
+    insta_submit = open_Browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button")
+    insta_submit.click()    
+    time.sleep(2)
+
+    ui.WebDriverWait(open_Browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".aOOlW.HoLwm"))).click()
+
+def open_profile(profile_id):
+    search_for = open_Browser.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input")
+    search_for.send_keys(profile_id)
+    time.sleep(3)
+
+    ant = open_Browser.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a")
+    time.sleep(3)
+    
+    ant.click()
+    time.sleep(3)
+
+def find_followers():
+    followers = open_Browser.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/ul/li[2]/a")
+    time.sleep(2)
+
+    followers.click()
+    time.sleep(4)
+
+    scroll_box = open_Browser.find_element_by_xpath("/html/body/div[4]/div/div[2]")
+    last_ht, ht = 0, 1
+    
+    
+    
+    while last_ht != ht:
+        last_ht = ht
+        time.sleep(1)
+        follower_links = scroll_box.find_elements_by_tag_name('a')
+        for link in follower_links:
+            follower_name = link.text
+            if(follower_name!=''):
+                print(follower_name)
+                followers_list.append(follower_name)
+        ht = open_Browser.execute_script("""
                 arguments[0].scrollTo(0, arguments[0].scrollHeight); 
                 return arguments[0].scrollHeight;
                 """, scroll_box)
 
-print(likers)
+def find_following():
+    following = open_Browser.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/ul/li[3]/a")
+    time.sleep(2)
 
-# following = open_Browser.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/ul/li[3]/a")
-# time.sleep(2)
+    following.click()
+    time.sleep(3)
 
-# following.click()
-# time.sleep(3)
+    scroll_box = open_Browser.find_element_by_xpath("/html/body/div[4]/div/div[2]")
+    last_ht, ht = 0, 1
 
-# scroll_box = open_Browser.find_element_by_xpath("/html/body/div[4]/div/div[2]")
-# last_ht, ht = 0, 1
+  
 
-# following_list = []
+    while last_ht != ht:
+        last_ht = ht
+        time.sleep(1)
+        following_links = scroll_box.find_elements_by_tag_name('a')
+        for link in following_links:
+            following_name = link.text
+            if(following_name!=''):
+                print(following_name)
+                following_list.append(following_name)
+        ht = open_Browser.execute_script("""
+                arguments[0].scrollTo(0, arguments[0].scrollHeight); 
+                return arguments[0].scrollHeight;
+                """, scroll_box)
 
-# while last_ht != ht:
-#     last_ht = ht
-#     time.sleep(1)
-#     following_links = scroll_box.find_elements_by_tag_name('a')
-#     for link in following_links:
-#         following_name = link.text
-#         if(following_name!=''):
-#             print(following_name)
-#             following_list.append(following_name)
-#     ht = open_Browser.execute_script("""
-#                 arguments[0].scrollTo(0, arguments[0].scrollHeight); 
-#                 return arguments[0].scrollHeight;
-#                 """, scroll_box)
+def find_likers():
+    recent_post = open_Browser.find_element_by_xpath("/html/body/div[1]/section/main/div/div[3]/article/div[1]/div/div[1]/div[1]/a/div/div[2]")
+    time.sleep(2)
+    recent_post.click()
+    time.sleep(2)
+    likes = open_Browser.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[2]/section[2]/div/div/button")
+    time.sleep(2)
+    likes.click()
+    time.sleep(3)
+    scroll_box = open_Browser.find_element_by_xpath("/html/body/div[5]/div/div[2]/div")
+    last_ht, ht = 0, 1
+   
+    while last_ht != ht:
+        last_ht = ht
+        time.sleep(1)
+        likers_links = scroll_box.find_elements_by_tag_name('a')
+        for link in likers_links:
+            likers_name = link.text
+            if(likers_name!=''):
+                print(likers_name)
+                likers.append(likers_name)
+        ht = open_Browser.execute_script("""
+                arguments[0].scrollTo(0, arguments[0].scrollHeight); 
+                return arguments[0].scrollHeight;
+                """, scroll_box)
 
-file1 = open('likers.txt', 'w')
-
-for i in likers:
-    file1.write(i + "\n")
-file1.close()
 
 
-# file2 = open('following.txt', 'w')
-# for i in following_list:
-#     file2.write(i+'\n')
-# file2.close()
+def write_2_file(filename, listname):
+    file1 = open(filename, 'w')
+    for i in listname:
+        file1.write(i + "\n")
+    file1.close()
 
+
+if __name__ == "__main__":
+    
+    profile = input("Targeted profile \n @")
+    user_name = input("Your username \n @")
+    password = input("Your password \n ")
+    open_Browser = webdriver.Chrome()
+    open_Browser.get('https://www.instagram.com/')
+    sleep(2)
+    login(user_name, password)
+    open_profile(profile)
+    find_followers()
+    write_2_file('him_followers', followers_list)
+    
+
+   
